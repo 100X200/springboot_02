f7a7504b0487ca3e960c8ba5afd6fd4c11663ad0
qp <qq1213579@126.com>
2018-11-23 21:07:17 +0800

    2

---
+++ src/main/java/com/baizhi/App.java
@@ -0,0 +1,59 @@
+package com.baizhi;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+
+import java.util.Properties;
+
+
+@SpringBootApplication
+@MapperScan(value = "com.baizhi.dao")
+public class App {
+    public static void main(String[] args) {
+
+        SpringApplication.run(App.class,args);
+    }
+    @Bean
+    //使用@Bean注入fastJsonHttpMessageConvert
+    public HttpMessageConverters fastJsonHttpMessageConverters(){
+        //1.需要定义一个Convert转换消息的对象
+        FastJsonHttpMessageConverter fastConverter=new FastJsonHttpMessageConverter();
+        //2.添加fastjson的配置信息，比如是否要格式化返回的json数据
+//
+        FastJsonConfig fastJsonConfig=new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        //3.在convert中添加配置信息
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+
+        HttpMessageConverter<?> converter=fastConverter;
+        return new HttpMessageConverters(converter);
+    }
+    @Bean
+    public DefaultKaptcha kaptcha() {
+        Properties properties = new Properties();
+        properties.put("kaptcha.border", "no");
+        properties.put("kaptcha.border.color", "105,179,90");
+        properties.put("kaptcha.textproducer.font.color", "green");
+        properties.put("kaptcha.image.width", "125");
+        properties.put("kaptcha.image.height", "45");
+        properties.put("kaptcha.textproducer.font.size", "45");
+        properties.put("kaptcha.session.key", "code");
+        properties.put("kaptcha.textproducer.char.length", "2");
+        properties.put("kaptcha.textproducer.font.names", "宋体,楷体,微软雅黑");
+        Config config = new Config(properties);
+        DefaultKaptcha defaultKaptcha = new DefaultKaptcha();
+        defaultKaptcha.setConfig(config);
+        return defaultKaptcha;
+    }
+
+
+}
